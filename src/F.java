52a19a96b2b6d13a5d765ffd169d0dae73a01ee2
andrey <mal_192@mail.ru>
2020-09-03 13:06:58 +0500

    working version correction

--- src/F.java
+++ src/F.java
@@ -3,12 +3,7 @@ import java.util.Scanner;
 public class F {
 
 public static boolean mas[][];
-public static void mas_z(int nn, int mm) {
-    if (mas[nn][mm] == true) mas[nn][mm] = false; else mas[nn][mm] = true;
-}
-
-
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         Scanner input_ = new Scanner(System.in);
         int n = input_.nextInt(); //рядов
         int m = input_.nextInt(); //элементов в ряде
@@ -19,41 +14,33 @@ public static void mas_z(int nn, int mm) {
         for (int nn=0; nn<n; nn++) //ряд
             for (int mm = 0; mm<m; mm++) { // позиция
                 boolean bin = false;
-                if (mas[nn][mm] == false) {
+                if (!mas[nn][mm]) {
                     if ((nn < n - 1) && (nn > 0)) {
-                        if (mas[nn - 1][mm] == true) bin = true;
-                        if (mas[nn + 1][mm] == true) bin = true;
+                        if (mas[nn - 1][mm]) bin = true;
+                        if (mas[nn + 1][mm]) bin = true;
                     } else if (nn == 0) {
-                        if (mas[nn + 1][mm] == true) bin = true;
-                    } else if (nn == n - 1) if (mas[nn - 1][mm] == true) bin = true;
+                        if (mas[nn + 1][mm]) bin = true;
+                    } else if (nn == n - 1) if (mas[nn - 1][mm]) bin = true;
 
                     if ((mm < m - 1) && (mm > 0)) {
-                        if (mas[nn][mm - 1] == true) bin = true;
-                        if (mas[nn][mm + 1] == true) bin = true;
+                        if (mas[nn][mm - 1]) bin = true;
+                        if (mas[nn][mm + 1]) bin = true;
                     } else if (mm == 0) {
-                        if (mas[nn][mm + 1] == true) bin = true;
-                    } else if (mm == m - 1) if (mas[nn][mm - 1] == true) bin = true;
+                        if (mas[nn][mm + 1]) bin = true;
+                    } else if (mm == m - 1) if (mas[nn][mm - 1]) bin = true;
 
-                    if (bin == false) {
-                      //  count_ = count_ + 1;
+                    if (!bin) {
+                        count_ = count_ + 1;
                         if ((nn < n - 1) && (nn > 0)) mas[nn + 1][mm] = true;
                         else if (mm<m-1) mas[nn][mm + 1] = true; else
-
-
-                       // if (((nn == 0) || (nn == nn - 1))&&(mm<m-1)) mas[nn][mm + 1] = true;
                         if ((nn == n-1)&&(mm ==m-1)) mas[nn][mm] = true; //последний элемент
                     }
-                } //nn = n-1; m=0
+                }
             }
-
-
-        count_=0;
         for (int nn=0; nn<n; nn++) { //ряд
-            System.out.println("");
+             System.out.println("");
             for (int mm = 0; mm < m; mm++) {
-                if (mas[nn][mm] == true){
-                   count_++;
-                    System.out.print(1); }else System.out.print(0);
+                if (mas[nn][mm])  System.out.print(1); else System.out.print(0);
                 System.out.print(' ');
             }
         }
